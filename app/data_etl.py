848c0d24f349cfed8af70faac07c5c620f1eb087
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2024-10-26 13:53:00 -0400

    Adds main file for the functions created

--- app/data_etl.py
+++ app/data_etl.py
@@ -1,39 +1,74 @@
 import csv
+import flask
 
-with open('../SAIDI_SAIFI_raw_FY24_Q4_v2.csv') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    line_count = 0
-    csv_reader = list(csv_reader)
-    #title = csv_reader[3]
-    period_table = {}
-    # starting point for tables locations
-    sp=3
-    ep=10
-    # end starting point
+def read_file(filepath):
+    with open(filepath) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        csv_reader = list(csv_reader)
+        #title = csv_reader[3]
+        period_table = {}
+        # starting point for tables locations
+        sp=3
+        ep=10
+        # end starting point
 
-    # Loop for col
-    for j in range(3, 78):
-        # define storage
-        period_month = ''
-        temp_list=[]
+        # Loop for col
+        for j in range(3, 78):
+            # define storage
+            period_month = ''
+            temp_list=[]
 
-        # loop for rows
-        for i in range(21):
-            if i == 3:
-                temp_table = csv_reader[i][sp:ep]
-                if len(temp_table) >0:
-                    temp_month =temp_table[0].rstrip()
-                    period_month = temp_month
-                    temp_list.append(temp_table)
-            elif i > 3:
-                temp_table = csv_reader[i][sp:ep]
-                if len(temp_table) >0:
-                    temp_list.append(temp_table)
-        period_table[period_month]=temp_list
-        # break
-        ep+=8
-        sp+=8
+            # loop for rows
+            for i in range(21):
+                if i == 3:
+                    temp_table = csv_reader[i][sp:ep]
+                    if len(temp_table) >0:
+                        temp_month =temp_table[0].rstrip()
+                        period_month = temp_month
+                        temp_list.append(temp_table)
+                elif i > 3:
+                    temp_table = csv_reader[i][sp:ep]
+                    if len(temp_table) >0:
+                        temp_list.append(temp_table)
+            period_table[period_month]=temp_list
+            # break
+            ep+=8
+            sp+=8
 
-    print(period_month)
-    print(period_table['Periodo de 06-01-2021 a 06-30-2021'])
-    print(period_table)
+        print(period_month)
+        print(period_table['Periodo de 06-01-2021 a 06-30-2021'])
+        print(period_table)
+
+        return period_table
+
+
+def save_period_table_to_csv(period_table, output_file):
+    """Saves the period_table dictionary to a CSV file.
+
+    Args:
+        period_table (dict): The dictionary containing period data.
+        output_file (str): The path to the output CSV file.
+    """
+    with open(output_file, mode='w', newline='') as file:
+        csv_writer = csv.writer(file)
+
+        # Write the header
+        csv_writer.writerow(['Period Month', 'Data'])
+
+        # Iterate over the period_table dictionary
+        for period_month, data_list in period_table.items():
+            # Write the period month
+            csv_writer.writerow([period_month])
+
+            # Write the data for the period month
+            for data in data_list:
+                csv_writer.writerow([''] + data)  # Indent data rows for clarity
+
+# Example usage
+period_table = {
+    'Periodo de 06-01-2021 a 06-30-2021': [['data1', 'data2'], ['data3', 'data4']],
+    'Periodo de 07-01-2021 a 07-31-2021': [['data5', 'data6'], ['data7', 'data8']]
+}
+
+save_period_table_to_csv(period_table, 'output.csv')
---
+++ app/save_data_to_File.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Main module to process CSV and save period table"""
+
+import data_etl
+
+def main():
+    # Define the input and output file paths
+    input_file = '../SAIDI_SAIFI_raw_FY24_Q4_v2.csv'
+    output_file = '../output.csv'
+
+    # Process the CSV file to create the period table
+    period_table = data_etl.read_file(input_file)
+
+    # Save the period table to a new CSV file
+    data_etl.save_period_table_to_csv(period_table, output_file)
+
+    print(f"Period table saved to {output_file}")
+
+if __name__ == '__main__':
+    main()
